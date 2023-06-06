a99707356fc358f163f065aa6098cc1a13281755
Sameer Donga <sameer@trillbit.com>
2023-06-06 16:19:47 +0530

    ZoomIn ZoomOut Completed

--- lib/components/show_full_image.dart
+++ lib/components/show_full_image.dart
@@ -4,15 +4,17 @@ import 'cached_network_image.dart';
 
 class ShowFullImage extends StatelessWidget {
   final String postImage;
-
   const ShowFullImage({Key? key, required this.postImage}) : super(key: key);
+
+  final double minScale = 1;
+  final double maxScale = 4;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.only(left:22,right: 22),
+          padding: const EdgeInsets.only(left: 22, right: 22),
           child: SingleChildScrollView(
             child: Column(
               children: [
@@ -46,23 +48,28 @@ class ShowFullImage extends StatelessWidget {
                 SizedBox(
                   height: 140,
                 ),
-                Container(
-                 // margin: EdgeInsets.symmetric(horizontal: 16),
-                  width: double.infinity,
-                  height: 300,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(12.0),
-                    child: postImage != null && postImage != ""
-                        ? CachedNetworkImageWidget(
+                InteractiveViewer(
+                  panEnabled: false,
+                  clipBehavior: Clip.none,
+                  minScale: minScale,
+                  maxScale: maxScale,
+                  child: Container(
+                    width: double.infinity,
+                    height: 300,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(12.0),
+                      child: postImage != null && postImage != ""
+                          ? CachedNetworkImageWidget(
                             image: postImage,
                             width: double.infinity,
                             height: 300,
                           )
-                        : Icon(
-                            Icons.error_outline,
-                            color: Colors.black,
-                            size: 30,
-                          ),
+                          : Icon(
+                              Icons.error_outline,
+                              color: Colors.black,
+                              size: 30,
+                            ),
+                    ),
                   ),
                 )
               ],
